540a39f3f5f1e8a9b2a6c438a54a8816dee5e836
free1112 <13633723956@163.com>
2026-03-19 19:10:01 +0800

    完善了项目的基本框架

--- src/main/java/org/example/recruit/service/StudentService.java
+++ src/main/java/org/example/recruit/service/StudentService.java
@@ -1,12 +1,9 @@
 package org.example.recruit.service;
 
-import com.baomidou.mybatisplus.extension.service.IService;
-import org.example.recruit.dto.StudentApplyDTO;
 import org.example.recruit.entity.Student;
 
 import java.util.List;
 
-public interface StudentService  {
-    boolean apply(StudentApplyDTO studentApplyDTO);
+public interface StudentService {
 
 }
